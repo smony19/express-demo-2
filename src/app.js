a62ee9f680e5f6f016b107888f994da4b5276891
smony19 <115245614+smony19@users.noreply.github.com>
2023-08-08 15:45:18 +0530

    demo-1

--- src/app.js
+++ src/app.js
@@ -1,1 +1,21 @@
+const express = require("express");
+const app = express();
 
+app.use("/static", express.static("public"));
+
+app.get("/", (req, res) => {
+  const msg = `This was a ${req.method} request from a browser.`;
+  res.send(msg);
+});
+
+function controller(req, res) {
+  const data = {
+    method: req.method,
+    num: Math.random(),
+    str: "Hello World!",
+  };
+  res.json(data);
+}
+
+app.get("/extra", controller);
+module.exports = app;
